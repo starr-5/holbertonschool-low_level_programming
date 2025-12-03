b31f7d1024bd2cbffe033b7b71b1e6d147dfa693
starr-5 <studying12567@gmail.com>
2025-12-03 09:13:28 +0000

    Correction

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
 int main(void)
 {
-	int i;
-	for (i=48; i=57; i++)
-		putchar(i);
-	putchar('\n');
-	return(0);
+    int i;
+
+    for (i = 48; i <= 57; i++)
+        putchar(i);       /* first putchar */
+
+    putchar('\n');        /* second putchar */
+
+    return (0);
 }
