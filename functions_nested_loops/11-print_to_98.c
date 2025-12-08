28381995bc5a83469e33d97c25783a95622a746c
starr-5 <studying12567@gmail.com>
2025-12-08 11:21:48 +0000

    Task 11 correction

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,22 +9,22 @@
  */
 void print_to_98(int n)
 {
-    if (n <= 98)
-    {
-        while (n < 98)
-        {
-            printf("%d, ", n);
-            n++;
-        }
-        printf("%d\n", 98);
-    }
-    else
-    {
-        while (n > 98)
-        {
-            printf("%d, ", n);
-            n--;
-        }
-        printf("%d\n", 98);
-    }
+	if (n <= 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		printf("%d\n", 98);
+	}
+	else
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		printf("%d\n", 98);
+	}
 }
