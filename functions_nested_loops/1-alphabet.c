f8c5dcdcd0c922dd993afaf3c7c2232917e382c2
starr-5 <studying12567@gmail.com>
2025-12-08 10:03:19 +0000

    Task 1: fix Betty style

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
+
 /**
  * print_alphabet - prints the alphabet in lowercase
  */
-
 void print_alphabet(void)
 {
 	char c;
 
-	for (c='a'; c<='z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
+
 	_putchar('\n');
 }
