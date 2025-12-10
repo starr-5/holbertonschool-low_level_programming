1bf1e2401ad656558e2d77ad6f5605fde61014cc
starr-5 <studying12567@gmail.com>
2025-12-10 06:54:05 +0000

    Task 5 correction

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string in place
+ * @s: the string to reverse
+ */
+void rev_string(char *s)
+{
+    int i = 0, j = 0;
+    char temp;
+
+    /* Find the length of the string */
+    while (s[j] != '\0')
+        j++;
+
+    j--; /* Move to the last valid character */
+
+    /* Swap characters from both ends */
+    while (i < j)
+    {
+        temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+
+        i++;
+        j--;
+    }
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 /*Prototype for reset_to_98 function */
+void rev_string(char *s);
 void reset_to_98(int *n);
 int _strlen(char *s);
 void _puts(char *str);
