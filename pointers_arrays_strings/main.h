9aa42ef8c4624de97ce819c5cec76807bb45c60d
starr-5 <studying12567@gmail.com>
2025-12-10 07:20:20 +0000

    Task 9 is completed

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 void reset_to_98(int *n);
 int _strlen(char *s);
 void _puts(char *str);
