ce19303bb4fdd450486c6e20405ed533a4e039a5
starr-5 <studying12567@gmail.com>
2025-12-12 13:29:19 +0000

    Task 4 is completed

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: pointer to the first element of the array
+ * @n: number of elements in the array
+ *
+ * Description: This function reverses the order of the elements in
+ * the array pointed to by @a. The first element becomes the last,
+ * the second becomes the second to last, and so on.
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i]
