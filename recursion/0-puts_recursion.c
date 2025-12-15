f85df0f65b43a0c45cdd9b0a9fdc69843414d9e5
starr-5 <studying12567@gmail.com>
2025-12-15 13:49:29 +0000

    Task 0 README file added

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,16 +2,16 @@
 
 /**
  * _puts_recursion - prints a string followed by a new line
- * @s: string to print
+ * @s: string to be printed
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
 
-    _putchar(*s);
-    _puts_recursion(s + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
