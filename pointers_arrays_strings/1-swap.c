3327fd941e003f2d9a391d1969a4f65e348f2bea
starr-5 <studying12567@gmail.com>
2025-12-16 18:03:03 +0000

    Betty style

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -4,12 +4,14 @@
  * swap_int - swaps the values of two integers
  * @a: pointer to first integer
  * @b: pointer to second integer
+ *
+ * Return: nothing
  */
 void swap_int(int *a, int *b)
 {
-    int temp;
+	int temp;
 
-    temp = *a;
-    *a = *b;
-    *b = temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
