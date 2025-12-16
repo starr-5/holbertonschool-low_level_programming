170b7de9fbed5663f695b46586fc2c7d611b154c
starr-5 <studying12567@gmail.com>
2025-12-16 18:11:34 +0000

    Betty style

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse
+ * print_rev - prints a string in reverse followed by a new line
  * @s: pointer to the string
+ *
+ * Return: nothing
  */
 void print_rev(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    /* find the length of the string */
-    while (s[i] != '\0')
-    {
-        i++;
-    }
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 
-    /* print the string in reverse */
-    for (i = i - 1; i >= 0; i--)
-    {
-        _putchar(s[i]);
-    }
-    _putchar('\n');
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
