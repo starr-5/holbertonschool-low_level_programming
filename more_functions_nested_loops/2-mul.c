5c363444034bb13d78a45ed5c8284ec6e7196928
starr-5 <studying12567@gmail.com>
2025-12-17 10:17:53 +0000

    Task 2 betty style

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the product of a and b
+ */
 int mul(int a, int b)
 {
-    return (a*b);
+    return (a * b);
 }
