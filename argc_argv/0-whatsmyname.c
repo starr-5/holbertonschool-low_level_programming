85fda8d080083abd05ae98be91719daeba54e937
starr-5 <studying12567@gmail.com>
2025-12-19 09:24:29 +0000

    Task 0 is completed

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
