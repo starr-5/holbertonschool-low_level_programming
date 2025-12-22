12dbe072fa1a1dfab4a5cd67061bc95fda75aba1
starr-5 <studying12567@gmail.com>
2025-12-22 07:40:48 +0000

    Betty style

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,19 +10,19 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    char *ptr;
-    unsigned int total, i;
+	unsigned int total, i;
+	char *ptr;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-    total = nmemb * size;
-    ptr = malloc(total);
-    if (ptr == NULL)
-        return (NULL);
+	total = nmemb * size;
+	ptr = malloc(total);
+	if (ptr == NULL)
+		return (NULL);
 
-    for (i = 0; i < total; i++)
-        ptr[i] = 0;
+	for (i = 0; i < total; i++)
+		ptr[i] = 0;
 
-    return (ptr);
+	return (ptr);
 }
