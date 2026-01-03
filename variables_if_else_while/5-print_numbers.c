a579bee68b7f449e844293f912c029fb2cd772ed
starr-5 <studying12567@gmail.com>
2026-01-03 11:10:08 +0000

    Betty

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
-int main (void)
+
+/**
+ * main - Prints all single digit numbers of base 10
+ *
+ * Return: Always 0
+ */
+int main(void)
 {
-	int i;
-	for (i=0; i<10; i++)
-		printf("%d", i);
-	printf("\n");
-	return(0);
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
 }
--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - Prints all single digit numbers of base 10
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    int i;
+	int n;
 
-    for (i = 48; i <= 57; i++)
-        putchar(i);       /* first putchar */
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + '0');
+	}
+	putchar('\n');
 
-    putchar('\n');        /* second putchar */
-
-    return (0);
+	return (0);
 }
