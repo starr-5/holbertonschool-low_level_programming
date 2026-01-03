d769c1ab02f49bee1efd35df3827edc54d76d7e5
starr-5 <studying12567@gmail.com>
2026-01-03 19:03:49 +0000

    Task 2 is completed

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,15 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stddef.h> /* for size_t and NULL */
+
+/* Task 0 */
 void print_name(char *name, void (*f)(char *));
+
+/* Task 1 */
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+/* Task 2 */
 int int_index(int *array, int size, int (*cmp)(int));
+
 #endif /* FUNCTION_POINTERS_H */
