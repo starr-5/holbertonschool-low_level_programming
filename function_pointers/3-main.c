b316983c5f7dbfe24e8ec43ca1c9e5763e72d07e
starr-5 <studying12567@gmail.com>
2026-01-03 19:13:47 +0000

    Task 3 is completed

---
+++ function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - calculator program
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	op = get_op_func(argv[2]);
+	if (op == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = op(a, b);
+	printf("%d\n", result);
+
+	return (0);
+}
