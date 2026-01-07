8d346e88e3351612c2970f153619a6b00cea64f3
starr-5 <studying12567@gmail.com>
2026-01-07 11:37:17 +0000

    Task 8 is completed

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square using the character '#'
+ * @size: size of the square
+ *
+ * Description: If size is 0 or less, prints only a newline.
+ * Each line of the square ends with a newline.
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
