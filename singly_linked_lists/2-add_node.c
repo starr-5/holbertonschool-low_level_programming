7f7993ab8f3f9149d3b8eb308803886978df2c14
starr-5 <studying12567@gmail.com>
2026-01-15 10:48:09 +0000

    fix

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,6 +12,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int len;
 
 	if (head == NULL || str == NULL)
 		return (NULL);
@@ -27,7 +28,10 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	for (len = 0; str[len]; len++)
+		;
+
+	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 
