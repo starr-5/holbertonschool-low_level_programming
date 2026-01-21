c39cd89152d4618ebb3914d7ae600f87d77443f4
starr-5 <studying12567@gmail.com>
2026-01-21 13:19:08 +0000

    Fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,40 +1,67 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <unistd.h>
+
+#define BUF_SIZE 1024
 
 /**
- * main - copies a file to another file
- * @argc: number of arguments
- * @argv: arguments
- * Return: 0 on success, exits on error
+ * main - copies the content of a file to another file
+ * @ac: argument count
+ * @av: argument vector
+ * Return: 0 on success, exits with 97-100 on failure
  */
-int main(int argc, char *argv[])
+int main(int ac, char **av)
 {
-	int fd_from, fd_to;
-	ssize_t r;
-	char buf[1024];
+	int fd_from, fd_to, r, w;
+	char buf[BUF_SIZE];
 
-	if (argc != 3)
-		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from < 0)
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to < 0)
-		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close(fd_from);
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
 
-	while ((r = read(fd_from, buf, 1024)) > 0)
-		if (write(fd_to, buf, r) != r)
-			dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while ((r = read(fd_from, buf, BUF_SIZE)) > 0)
+	{
+		w = write(fd_to, buf, r);
+		if (w != r)
+		{
+			close(fd_from);
+			close(fd_to);
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
 
-	if (r < 0)
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	if (r == -1)
+	{
+		close(fd_from);
+		close(fd_to);
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 
-	if (close(fd_from) < 0 || close(fd_to) < 0)
-		dprintf(2, "Error: Can't close fd\n"), exit(100);
+	if (close(fd_from) == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (close(fd_to) == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fd_to), exit(100);
 
 	return (0);
 }
