8decf921fc74a45e92e3d135568ae2774576d8d4
starr-5 <studying12567@gmail.com>
2026-01-21 13:00:38 +0000

    fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,31 +11,31 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, written = 0;
-    int i = 0;
+	int fd, written = 0;
+	int i = 0;
 
-    if (!filename)
-        return (-1);
+	if (!filename)
+		return (-1);
 
-    /* open file for writing, create if missing, truncate if exists, permissions rw------- */
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
+	/* open file for writing, create if missing, truncate if exists */
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-    /* if text_content is not NULL, write it */
-    if (text_content)
-    {
-        while (text_content[i]) /* count length of string */
-            i++;
+	/* write text_content if it is not NULL */
+	if (text_content)
+	{
+		while (text_content[i])
+			i++;
 
-        written = write(fd, text_content, i);
-        if (written != i) /* check if write was successful */
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+		written = write(fd, text_content, i);
+		if (written != i)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-    close(fd); /* close the file */
-    return (1); /* success */
+	close(fd);
+	return (1);
 }
