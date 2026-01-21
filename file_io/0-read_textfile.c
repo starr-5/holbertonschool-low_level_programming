182c55b6c85efaa14db3956e1a0c163a24499f4c
starr-5 <studying12567@gmail.com>
2026-01-21 12:16:20 +0000

    betty doc style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,7 +2,13 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include "main.h"
-
+/**
+ * read_textfile - reads a text file and prints it to standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: number of letters printed, or 0 on failure
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
--- file_io/main.h
+++ file_io/main.h
@@ -2,7 +2,13 @@
 #define MAIN_H
 
 #include <sys/types.h>
-
+/**
+ * read_textfile - reads a text file and prints it to standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: number of letters printed, or 0 on failure
+ */
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
